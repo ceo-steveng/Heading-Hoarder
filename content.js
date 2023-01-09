f6626aa1792348bc7b747503243703e9b5d7c2a0
coachstevengarcia <coachstevengarcia@gmail.com>
2023-01-09 12:22:22 -0600

    Updated code in manifest file

--- content.js
+++ content.js
@@ -1,20 +1,8 @@
-// Listen for messages from the popup
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.message === 'copy') {
-      // Get the H1, H2, H3, and H4 titles on the page
-      var headings = document.querySelectorAll('h1, h2, h3, h4');
-      var headingTexts = [];
-  
-      // Add the text of each heading to the headingTexts array
-      for (var i = 0; i < headings.length; i++) {
-        headingTexts.push(headings[i].textContent);
-      }
-  
-      // Join the heading texts into a single string, separated by newlines
-      var headingString = headingTexts.join('\n');
-  
-      // Copy the heading string to the clipboard
-      navigator.clipboard.writeText(headingString);
-    }
+// Listen for messages from the page action
+chrome.pageAction.onClicked.addListener(function(tab) {
+  // Send a message to the active tab
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {'message': 'copy'});
   });
-  
+});
